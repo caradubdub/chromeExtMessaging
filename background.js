30d6736045d86718663bbae572f3ced52e18bb73
Trevor Carr <trevor.a.carr@gmail.com>
2021-03-22 22:49:20 -0400

    set up initial tab

--- background.js
+++ background.js
@@ -1,22 +1,53 @@
 console.log("<----- Extension script started running ----->");
 
-window.perfWatch = {};
+const connections = {};
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  window.perfWatch[sender.tab.id] = message.essential || null;
-  console.log("in listener");
-});
+chrome.runtime.onConnect.addListener((port) => {
+  // assign the listener function to a variable so we can remove it later
+  const devToolsListener = (message, sender, sendResponse) => {
 
-window.addEventListener("DOMContentLoaded", () => {
-  let bg = chrome.extension.getBackgroundPage();
-
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    let currentTabId = tabs[0].id;
-    let currentPerf = bg.perfWatch[currentTabId];
-
-    // safety check: when page is still loading
-    if (!currentPerf) {
+    // creates a new key/value pair of current window & devtools tab when a new devtools tab is opened
+    if (message.name === 'connect' && message.tabId) {
+      connections[message.tabId] = port;
       return;
     }
-  });
+    // may not be necessary, this is an attempt to keep the port alive
+    return true;
+  };
+
+  if (port.name === 'React Fetch Tree') port.onMessage.addListener(devToolsListener);
+  console.log("connections in background.js", connections);
 });
+
+
+chrome.runtime.onMessage.addListener(function (req, sender) {
+  if (sender.tab) {
+    let tabId = sender.tab.id;
+    if (tabId in connections) {
+      connections[tabId].postMessage(req);
+    } else console.log('WARNING:: Tab not found in connection list');
+  } else console.log('WARNING:: sender.tab not defined');
+  // see this for why https://github.com/mozilla/webextension-polyfill/issues/130#issue-333539552
+  return Promise.resolve('Dummy response to keep the console quiet');
+});
+
+// window.perfWatch = {};
+
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//   window.perfWatch[sender.tab.id] = message.essential || null;
+//   console.log("in listener");
+// });
+
+// window.addEventListener("DOMContentLoaded", () => {
+//   let bg = chrome.extension.getBackgroundPage();
+
+//   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+//     let currentTabId = tabs[0].id;
+//     let currentPerf = bg.perfWatch[currentTabId];
+
+//     // safety check: when page is still loading
+//     if (!currentPerf) {
+//       return;
+//     }
+//   });
+// });
